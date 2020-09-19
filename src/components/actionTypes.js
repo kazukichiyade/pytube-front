edd7e65880ddb0c5e9cb6168f504080f760b9a88
kazukichi <zexyl_x@icloud.com>
2020-09-19 21:19:17 +0900

    actionを明確に分けた

---
+++ src/components/actionTypes.js
@@ -0,0 +1,10 @@
+// バックエンドのAPIにアクセス開始
+export const START_FETCH = "START_FETCH";
+// アクセス成功
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+// アクセス失敗
+export const ERROR_CATCHED = "ERROR_CATCHED";
+// LOGIN FORMのtextが変更される度に呼び出される
+export const INPUT_EDIT = "INPUT_EDIT";
+// ログインと新規作成の切り替え
+export const TOGGLE_MODE = "TOGGLE_MODE";
